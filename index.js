5d39bc1fd25b4ed13f2bc5066f405413c0101941
amelia.risner0@gmail.com <arisner@berkeley.edu>
2022-01-12 16:26:33 -0800

    commit tree 6f365cc832ee115740e475a2a4fa1dc44b875b76

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(array, string) {
+    return array.filter(function (elem) {
+        return elem.toLowerCase() === string.toLowerCase();
+    });
+}
+
+function fuzzyMatch(array, string) {
+    const end = string.length
+    return array.filter(function (elem) {
+        return elem.slice(0, end) === string;
+    });
+}
+
+function matchName(array, string) {
+    return array.filter(function (elem) {
+        return elem.name === string;
+    })
+}
